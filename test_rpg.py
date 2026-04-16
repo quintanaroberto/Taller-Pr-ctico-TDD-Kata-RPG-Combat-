30dc535ebc3299ec71c1be84e45ceee92e8cb07d
quintanaroberto <157411266+quintanaroberto@users.noreply.github.com>
2026-04-16 16:15:55 -0600

    Fase 2, paso 3

--- test_rpg.py
+++ test_rpg.py
@@ -19,3 +19,11 @@ def test_personaje_recibe_dano():
     # Assert
     assert heroe.hp == 800
 
+def test_personaje_muere_si_hp_llega_a_cero():
+    heroe = Personaje()
+    enemigo = Personaje()
+
+    enemigo.atacar(heroe, dano=1500)
+
+    assert heroe.hp == 0  # El HP no debe quedar en -500
+    assert heroe.esta_vivo == False
