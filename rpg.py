23be4fe8d655bd50654c5e19499598224b502840
quintanaroberto <157411266+quintanaroberto@users.noreply.github.com>
2026-04-16 16:16:28 -0600

    Fase 2, paso 4

--- rpg.py
+++ rpg.py
@@ -7,3 +7,8 @@ class Personaje:
     def atacar(self, objetivo, dano):
         objetivo.hp -= dano
 
+    def atacar(self, objetivo, dano):
+        objetivo.hp -= dano
+        if objetivo.hp <= 0:
+            objetivo.hp = 0
+            objetivo.esta_vivo = False
