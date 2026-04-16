97466646d6ebb85fa84aee08fac7d8e70364c411
quintanaroberto <157411266+quintanaroberto@users.noreply.github.com>
2026-04-16 16:13:01 -0600

    Fase 2, paso 1

--- test_rpg.py
+++ test_rpg.py
@@ -9,3 +9,13 @@ def test_personaje_nace_con_estadisticas_correctas():
     assert heroe.nivel == 1
     assert heroe.esta_vivo == True
 
+def test_personaje_recibe_dano():
+    heroe = Personaje()
+    enemigo = Personaje()
+
+    # Act (Actuar)
+    enemigo.atacar(heroe, dano=200)
+
+    # Assert
+    assert heroe.hp == 800
+
